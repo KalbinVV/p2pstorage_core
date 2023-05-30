80e0d614bc976835a34ae149542e3b144a57993d
Kalbin Vladimir <kalbin.wow@yandex.ru>
2023-05-31 03:09:16 +0500

    Moved storage server to separator project

--- p2pstorage_core/server/PackageHandler.py
+++
@@ -1,6 +0,0 @@
-from p2pstorage_core.server.Package import AbstractPackage
-
-
-class AbstractPackageHandler:
-    def handle_package(self, package: AbstractPackage) -> None:
-        raise Exception('Method should be overriden!')
