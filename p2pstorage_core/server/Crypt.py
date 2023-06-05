d874827fe19ff782f17dc74ece68b7743f729c51
Kalbin Vladimir <kalbin.wow@yandex.ru>
2023-06-06 04:39:15 +0500

    Hash of file function

--- p2pstorage_core/server/Crypt.py
+++ p2pstorage_core/server/Crypt.py
@@ -1,6 +1,24 @@
+import hashlib
 from enum import IntEnum
 
 
 class CryptMode(IntEnum):
     UNENCRYPTED = 1
     RSA = 2
+
+
+def get_hash_of_file(file_path: str) -> str:
+    buf_size = 65536  # 64kb chunks
+
+    sha256 = hashlib.sha3_256()
+
+    with open(file_path, 'rb') as f:
+        while True:
+            data = f.read(buf_size)
+
+            if not data:
+                break
+
+            sha256.update(data)
+
+    return sha256.hexdigest()
